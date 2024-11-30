760ddf0a20e51a994f3dfeac7895eaa70cb3ce05
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2024-11-30 21:34:37 +0100

    feat: Update SQL queries to use parameterized arguments for loggroup and profile, and add GetLogsOfPod query for enhanced log retrieval

--- db/queries.sql
+++ db/queries.sql
@@ -11,16 +11,17 @@ INSERT INTO logs (event_time,profile,loggroup, namespace_name, pod_name, contain
 -- name: GetLogs :many
 SELECT * FROM logs 
 WHERE event_time >= sqlc.arg(begindate) and event_time <= sqlc.arg(enddate)
-    AND loggroup = ?
-    AND profile = ?
+    AND loggroup = sqlc.arg(loggroup)
+    AND profile = sqlc.arg(profile)
 ORDER BY event_time;
+
+-- name: GetLogsOfPod :many
+SELECT * FROM logs 
+WHERE event_time >= sqlc.arg(begindate) and event_time <= sqlc.arg(enddate)
+    AND loggroup = sqlc.arg(loggroup)
+    AND profile = sqlc.arg(profile)
+    AND pod_name like sqlc.arg(pod_name)
+    ORDER BY event_time;
 
 -- name: CountLogs :one
 SELECT COUNT(*) FROM logs;
-
--- name: GetLogsByPod :many
-SELECT * FROM logs 
-    WHERE pod_name = ?
-        AND event_time >= ?
-        AND event_time <= ? 
-    ORDER BY event_time DESC;
